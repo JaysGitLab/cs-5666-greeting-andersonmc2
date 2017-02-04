216521ecd97b858eeade7c7cfb982873691abaf9
Mikeal Anderson <andersonmc2@appstate.edu>
2017-02-04 02:20:49 -0500

    Added command line name personalization

--- Greeting.java
+++ Greeting.java
@@ -1,5 +1,10 @@
 class Greeting{
     public static void main (String[] args){
-        System.out.println("Hello, World!");
+        if(args.length != 0){
+            System.out.println("Hello, " + args[0]);
+        }
+        else{
+            System.out.println("Hello, Mikeal!");
+        }
     }
 }
